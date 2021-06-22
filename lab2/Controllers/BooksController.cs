f257b32f2d2c3614fe0a47a462e50b13e911d7bf
congdanh1405 <trancongdanh1405@gmail.com>
2021-06-22 14:12:02 +0700

    them file

---
+++ lab2/Controllers/BooksController.cs
@@ -0,0 +1,102 @@
+﻿using lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        private List<Book> listBooks;
+        public BooksController()
+        {
+            listBooks = new List<Book>();
+            listBooks.Add(new Book()
+            {
+                id=1,
+                Title="Sach 1",
+                Author= "Tac gia sach 1",
+                PublicYear= 2017,
+                Price =40000,
+                Cover="Content/image/TH006.jpg"
+            });
+            listBooks.Add(new Book()
+            {
+                id = 2,
+                Title = "Sach 2",
+                Author = "Tac gia sach 2",
+                PublicYear = 2018,
+                Price = 50000,
+                Cover = "Content/image/TH007.jpg"
+            });
+            listBooks.Add(new Book()
+            {
+                id = 3,
+                Title = "Sach 3",
+                Author = "Tac gia sach 3",
+                PublicYear = 2019,
+                Price = 1000000,
+                Cover = "Content/image/TH008.jpg"
+            });
+        }
+        public ActionResult ListBooks()
+        {
+            ViewBag.TitlePagename = "Book view page";
+            return View(listBooks);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if(id==null)
+            {
+                return HttpNotFound();
+            }
+            Book book = listBooks.Find(s => s.id == id);
+            if (book == null)
+                return HttpNotFound();
+            return View(book);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = listBooks.Find(s => s.id == id);
+            if (book == null)
+                return HttpNotFound();
+            return View(book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var editBook = listBooks.Find(b => b.id == book.id);
+                    editBook.Title = book.Title;
+                    editBook.Author = book.Author;
+                    editBook.Cover = book.Cover;
+                    editBook.Price = book.Price;
+                    editBook.PublicYear = book.PublicYear;
+                    return View("ListBooks", listBooks);
+
+                }
+                catch(Exception ex)
+                {
+                    //log exception ex
+                    return HttpNotFound();
+                }
+            } 
+            else
+            {
+                ModelState.AddModelError("", "Input Model Not Valid");
+                return View(book);
+            }    
+        }
+    }
+}
---
+++ lab2/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab2.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "please input Title")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "please input Author")]
+        [StringLength(50, ErrorMessage ="Author les than 50 characters")]
+        public string Author { get; set; }
+        public int PublicYear { get; set; }
+        public double Price { get; set; }
+        public string Cover { get; set; }
+    }
+}
